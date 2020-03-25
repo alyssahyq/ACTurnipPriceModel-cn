e5caca033cd37003f9556e03f4da09664ac4c8a6
InsulatingShell <62570877+InsulatingShell@users.noreply.github.com>
2020-03-25 01:32:25 -0400

    Update Mar23.py
    版本：v 1.1
    最后更新时间： Mar 25
    感谢微博 @这个昵称还没有人注册 的反馈，现在已经考虑了每日上午和下午价格不同的情况

--- Mar23.py
+++ Mar23.py
@@ -5,9 +5,9 @@ info = '''白萝卜价格趋势模型(简体中文)
 Github: InsulatingShell
 https://github.com/InsulatingShell/ACTurnipPriceModel-cn/
 微博：@绝缘壳
-版本：v 1.0.1
+版本：v 1.1
 最后更新时间： Mar 25
-修复了 issue 第一次输入范围错误会出现异常 #1 的问题
+感谢微博 @这个昵称还没有人注册 的反馈，现在已经考虑了每日上午和下午价格不同的情况
 '''
 
 
@@ -28,26 +28,32 @@ def SunPriceCheck(price):
     return price
 
 def priceCheck(price):
-    if float(price) < 1 :
+    while float(price) < 0 :
         print("白萝卜的价格应该是一个正整数")
         new_price = readInput()
         if new_price == price:
-            return new_price
+            price = new_price
+            break
         else:
-            SunPriceCheck(new_price)
-    else:
-        return price
+            price = new_price
+    return price
 
 class Turnip:
     sun = -1
-    mon = -1
-    tue = -1
-    wed = -1
-    thu = -1
-    fri = -1
-    sat = -1
-    dec = 0
-    inc = 0
+    mon_am = -1
+    mon_pm = -1
+    tue_am = -1
+    tue_pm = -1
+    wed_am = -1
+    wed_pm = -1
+    thu_am = -1
+    thu_pm = -1
+    fri_am = -1
+    fri_pm = -1
+    sat_am = -1
+    sat_pm = -1
+    inc = 0 #记录是否上涨
+    dec = 0 #记录连续下跌次数
     fir = 0
     sec = 0
     thr = 0
@@ -57,18 +63,22 @@ class Turnip:
     flag_3 = False
     flag_4 = False
     lock_fluc = False
+    #期望
     expe_fluc = 0
     expe_3 = 0
     expe_4 = 0
-    today = -1
-    yesterday = -1
+    now = -1
+    last = -1
+    lock_3 = False
+    lock_4 = False
+
     def makePrediction(self):
         if Turnip.lock_fluc:
             Turnip.flag_fluc = True
             Turnip.flag_decr = False
             Turnip.flag_3 = False
             Turnip.flag_4 = False
-        if Turnip.inc == 0 and Turnip.dec >=4 :
+        if Turnip.inc == 0 and Turnip.dec >=8 :
             Turnip.flag_decr = True
             Turnip.flag_3 = False
             Turnip.flag_4 = False
@@ -90,11 +100,11 @@ class Turnip:
             if Turnip.flag_decr or Turnip.flag_fluc or Turnip.flag_4:
                 print("目前可能是三期型，", end='')
             else:
-                lock_3 = True
+                Turnip.lock_3 = True
                 print("目前就是三期型，", end='')
             print("则最大卖价范围在", Turnip.expe_3, "至", int(6 * int(Turnip.sun)), "之间")
             if Turnip.thr != 0:
-                if lock_3:
+                if Turnip.lock_3:
                     print("今天就是三期型的售价峰值，请卖出")
                 else:
                     print("今天可能是三期型的售价峰值，可以考虑卖出")
@@ -103,11 +113,11 @@ class Turnip:
             if Turnip.flag_decr or Turnip.flag_fluc or Turnip.flag_3:
                 print("目前可能是四期型，", end='')
             else:
-                lock_4 = True
+                Turnip.lock_4 = True
                 print("目前就是四期型，", end='')
             print("则最大卖价范围在", Turnip.expe_4, "至", int(2 * int(Turnip.sun)), "之间")
             if Turnip.fou != 0:
-                if lock_4:
+                if Turnip.lock_4:
                     print("今天就是四期型的售价峰值，请卖出")
                 else:
                     print("今天可能是四期型的售价峰值，可以考虑卖出")
@@ -118,7 +128,7 @@ class Turnip:
                 print("目前就是递减型，只会越来越便宜，我建议你现在就卖了")
 
     def roughModel(self):
-        X = (float(Turnip.mon)/float(Turnip.sun))*100
+        X = (float(Turnip.mon_am)/float(Turnip.sun))*100
         if  91 <= X <= 100:
             #print('可能是"波动型"或"4期型"')
             Turnip.flag_fluc = True
@@ -157,18 +167,21 @@ class Turnip:
                     if Turnip.flag_4 and Turnip.flag_3 == False and Turnip.fir != 0 and Turnip.sec != 0 and Turnip.thr != 0 and Turnip.fou ==0 :
                         Turnip.fou = Turnip.today
 
-    def compareYesterday(self):
-        if (float(Turnip.yesterday) <= float(Turnip.today)):
+    def compareLastPrice(self):
+        if (float(Turnip.last) >= float(Turnip.now)):
             if Turnip.inc == 0: #连续下跌
                 Turnip.dec = Turnip.dec + 1
-            else:
-                Turnip.inc = Turnip.inc + 1
+            #else:
+                #Turnip.inc = Turnip.inc + 1
         else:
+            #print("if (",float(Turnip.last)," <= ",float(Turnip.now),",:")
             Turnip.inc = Turnip.inc + 1
-        if Turnip.dec == 3:
+        if Turnip.dec == 8: #周四下午都没变调
+            print("周四下午都没发生变调吗？情况不妙，趁早离手")
             Turnip.flag_4 = False
-        if Turnip.dec == 4:
             Turnip.flag_3 = False
+        #print("inc = ", Turnip.inc)
+        #print("dec = ",Turnip.dec)
 
 
     def Sun(self):
@@ -179,76 +192,153 @@ class Turnip:
         Turnip.today = Turnip.sun
         print('')
 
-    def Mon(self):
-        Turnip.yeseterday = Turnip.today
-        print('请输入周一收购价，回车结束输入')
-        mon_raw = readInput()
-        Turnip.mon = priceCheck(mon_raw)
-        print('周一买入价为：', Turnip.mon)
-        Turnip.today = Turnip.mon
+    def Mon_AM(self):
+        Turnip.last = Turnip.now
+        print('请输入周一上午收购价，回车结束输入')
+        raw_input = readInput()
+        Turnip.mon_am = priceCheck(raw_input) #
+        print('周一上午收购价为：', Turnip.mon_am) #
+        Turnip.now = Turnip.mon_am #
         Turnip.roughModel(self)
         Turnip.makePrediction(self)
-        if(Turnip.mon <= Turnip.sun):
+        if(float(Turnip.mon_am) <= float(Turnip.sun)):
             Turnip.dec = Turnip.dec+1
         else:
             Turnip.lock_fluc = True
         print('')
 
-    def Tue(self):
-        Turnip.yeseterday = Turnip.today
-        print('请输入周二收购价，回车结束输入')
-        tue_raw = readInput()
-        Turnip.tue = priceCheck(tue_raw)
-        print('周二买入价为：', Turnip.tue)
-        Turnip.today = Turnip.tue
-        Turnip.compareYesterday(self)
+    def Mon_PM(self):
+        Turnip.last = Turnip.now
+        print('请输入周一下午收购价，回车结束输入')
+        raw_input = readInput()
+        Turnip.mon_pm = priceCheck(raw_input)  #
+        print('周一下午收购价为：', Turnip.mon_pm)  #
+        Turnip.now = Turnip.mon_pm  #
+        Turnip.compareLastPrice(self)
         Turnip.recordPrice(self)
         Turnip.makePrediction(self)
+        print('')
 
-    def Wed(self):
-        Turnip.yeseterday = Turnip.today
-        print('请输入周三收购价，回车结束输入')
-        wed_raw = readInput()
-        Turnip.wed = priceCheck(wed_raw)
-        print('周三买入价为：', Turnip.wed)
-        Turnip.today = Turnip.wed
-        Turnip.compareYesterday(self)
+    def Tue_AM(self):
+        Turnip.last = Turnip.now
+        print('请输入周二上午收购价，回车结束输入')
+        raw_input = readInput()
+        Turnip.tue_am = priceCheck(raw_input)  #
+        print('周二上午收购价为：', Turnip.tue_am)  #
+        Turnip.now = Turnip.tue_am  #
+        Turnip.compareLastPrice(self)
         Turnip.recordPrice(self)
         Turnip.makePrediction(self)
+        print('')
 
-    def Thu(self):
-        Turnip.yeseterday = Turnip.today
-        print('请输入周四收购价，回车结束输入')
-        thu_raw = readInput()
-        Turnip.thu = priceCheck(thu_raw)
-        print('周四买入价为：', Turnip.thu)
-        Turnip.today = Turnip.thu
-        Turnip.compareYesterday(self)
+    def Tue_PM(self):
+        Turnip.last = Turnip.now
+        print('请输入周二下午收购价，回车结束输入')
+        raw_input = readInput()
+        Turnip.tue_pm = priceCheck(raw_input)  #
+        print('周二下午收购价为：', Turnip.tue_pm)  #
+        Turnip.now = Turnip.tue_pm  #
+        Turnip.compareLastPrice(self)
         Turnip.recordPrice(self)
         Turnip.makePrediction(self)
+        print('')
 
-    def Fri(self):
-        Turnip.yeseterday = Turnip.today
-        print('请输入周五收购价，回车结束输入')
-        fri_raw = readInput()
-        Turnip.fri = priceCheck(fri_raw)
-        print('周五买入价为：', Turnip.fri)
-        Turnip.today = Turnip.fri
-        Turnip.compareYesterday(self)
+    def Wed_AM(self):
+        Turnip.last = Turnip.now
+        print('请输入周三上午收购价，回车结束输入')
+        raw_input = readInput()
+        Turnip.wed_am = priceCheck(raw_input)  #
+        print('周三上午收购价为：', Turnip.wed_am)  #
+        Turnip.now = Turnip.wed_am  #
+        Turnip.compareLastPrice(self)
         Turnip.recordPrice(self)
         Turnip.makePrediction(self)
+        print('')
 
-    def Sat(self):
-        Turnip.yeseterday = Turnip.today
-        print('请输入周六收购价，回车结束输入')
-        sat_raw = readInput()
-        Turnip.sat = priceCheck(sat_raw)
-        print('周六买入价为：', Turnip.sat)
-        Turnip.today = Turnip.sat
-        Turnip.compareYesterday(self)
+    def Wed_PM(self):
+        Turnip.last = Turnip.now
+        print('请输入周三下午收购价，回车结束输入')
+        raw_input = readInput()
+        Turnip.wed_pm = priceCheck(raw_input)  #
+        print('周三下午收购价为：', Turnip.wed_pm)  #
+        Turnip.now = Turnip.wed_pm  #
+        Turnip.compareLastPrice(self)
         Turnip.recordPrice(self)
         Turnip.makePrediction(self)
-        print('不过今天都周六了，无论如何都请卖了，否则全烂了')
+        print('')
+
+    def Thu_AM(self):
+        Turnip.last = Turnip.now
+        print('请输入周四上午收购价，回车结束输入')
+        raw_input = readInput()
+        Turnip.thu_am = priceCheck(raw_input)  #
+        print('周四上午收购价为：', Turnip.thu_am)  #
+        Turnip.now = Turnip.thu_am  #
+        Turnip.compareLastPrice(self)
+        Turnip.recordPrice(self)
+        Turnip.makePrediction(self)
+        print('')
+
+    def Thu_PM(self):
+        Turnip.last = Turnip.now
+        print('请输入周四下午收购价，回车结束输入')
+        raw_input = readInput()
+        Turnip.thu_pm = priceCheck(raw_input)  #
+        print('周四下午收购价为：', Turnip.thu_pm)  #
+        Turnip.now = Turnip.thu_pm  #
+        Turnip.compareLastPrice(self)
+        Turnip.recordPrice(self)
+        Turnip.makePrediction(self)
+        print('')
+
+    def Fri_AM(self):
+        Turnip.last = Turnip.now
+        print('请输入周五上午收购价，回车结束输入')
+        raw_input = readInput()
+        Turnip.fri_am = priceCheck(raw_input)  #
+        print('周五上午收购价为：', Turnip.fri_am)  #
+        Turnip.now = Turnip.fri_am  #
+        Turnip.compareLastPrice(self)
+        Turnip.recordPrice(self)
+        Turnip.makePrediction(self)
+        print('')
+
+    def Fri_PM(self):
+        Turnip.last = Turnip.now
+        print('请输入周五下午收购价，回车结束输入')
+        raw_input = readInput()
+        Turnip.fri_pm = priceCheck(raw_input)  #
+        print('周五下午收购价为：', Turnip.fri_pm)  #
+        Turnip.now = Turnip.fri_pm  #
+        Turnip.compareLastPrice(self)
+        Turnip.recordPrice(self)
+        Turnip.makePrediction(self)
+        print('')
+
+    def Sat_AM(self):
+        Turnip.last = Turnip.now
+        print('请输入周六上午收购价，回车结束输入')
+        raw_input = readInput()
+        Turnip.sat_am = priceCheck(raw_input)  #
+        print('周六上午收购价为：', Turnip.sat_am)  #
+        Turnip.now = Turnip.sat_am  #
+        Turnip.compareLastPrice(self)
+        Turnip.recordPrice(self)
+        Turnip.makePrediction(self)
+        print('')
+
+    def Sat_PM(self):
+        Turnip.last = Turnip.now
+        print('请输入周六下午收购价，回车结束输入')
+        raw_input = readInput()
+        Turnip.sat_pm = priceCheck(raw_input)  #
+        print('周六下午收购价为：', Turnip.sat_pm)  #
+        Turnip.now = Turnip.sat_pm  #
+        Turnip.compareLastPrice(self)
+        Turnip.recordPrice(self)
+        Turnip.makePrediction(self)
+        print('不过现在都周六下午了，无论如何都请卖了，否则全烂了')
+        print('')
 
 
 def main():
@@ -256,12 +346,18 @@ def main():
         turnip = Turnip()
         print(info)
         turnip.Sun()
-        turnip.Mon()
-        turnip.Tue()
-        turnip.Wed()
-        turnip.Thu()
-        turnip.Fri()
-        turnip.Sat()
+        turnip.Mon_AM()
+        turnip.Mon_PM()
+        turnip.Tue_AM()
+        turnip.Tue_PM()
+        turnip.Wed_AM()
+        turnip.Wed_PM()
+        turnip.Thu_AM()
+        turnip.Thu_PM()
+        turnip.Fri_AM()
+        turnip.Fri_PM()
+        turnip.Sat_AM()
+        turnip.Sat_PM()
     except EOFError:
         exit(0)
 
